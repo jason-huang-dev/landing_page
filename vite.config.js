97a9d10d5eb055bf9b6e3d59ef531ef12659d442
jasonh232 <jason232013@gmail.com>
2024-07-25 15:46:14 -0400

    adding github pages configs

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/jason-huang-dev.github.io/",
+  base: "/landing-page/",
   server: {
     port: 5173,
     open: true,
